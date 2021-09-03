e84915b65ef0af7b66ce5419e843aafa8fcf0e86
Max <max.work.dev@gmail.com>
2021-09-03 07:17:32 +0300

    feat: change url

--- src/components/pages/PageProductForm/PageProductForm.tsx
+++ src/components/pages/PageProductForm/PageProductForm.tsx
@@ -1,11 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import {Product, ProductSchema} from "models/Product";
-import {Formik, Field, FormikProps, FormikValues} from 'formik';
-import {TextField} from 'formik-material-ui';
+import { Product, ProductSchema } from "models/Product";
+import { Formik, Field, FormikProps, FormikValues } from 'formik';
+import { TextField } from 'formik-material-ui';
 import axios from 'axios';
-import {useHistory, useParams} from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import PaperLayout from "components/PaperLayout/PaperLayout";
 import Typography from "@material-ui/core/Typography";
 import API_PATHS from "constants/apiPaths";
@@ -102,13 +102,13 @@ const emptyValues: any = ProductSchema.cast();
 
 export default function PageProductForm() {
   const history = useHistory();
-  const {id} = useParams<{ id: string }>();
+  const { id } = useParams<{ id: string }>();
   const [product, setProduct] = useState<Product | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const onSubmit = (values: FormikValues) => {
     const formattedValues = ProductSchema.cast(values);
-    const productToSave = id ? {...ProductSchema.cast(formattedValues), id} : formattedValues;
+    const productToSave = id ? { ...ProductSchema.cast(formattedValues), id } : formattedValues;
     axios.put(`${API_PATHS.bff}/product`, productToSave)
       .then(() => history.push('/admin/products'));
   };
